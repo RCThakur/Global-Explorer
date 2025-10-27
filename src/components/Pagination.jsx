f9fc8e44eec0f561f5028b4df23ae48c85eaf09c
RCThakur <rinkeshthakur1997@gmail.com>
2025-10-27 13:45:54 +0530

    Add Pagination component for country list navigation

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+export default function Pagination({ page, totalPages, onPageChange }) {
+  return (
+    <div className="pagination">
+      <button onClick={() => onPageChange(page - 1)} disabled={page <= 1}>
+        Prev
+      </button>
+      <span>
+        Page {page} of {totalPages}
+      </span>
+      <button
+        onClick={() => onPageChange(page + 1)}
+        disabled={page >= totalPages}
+      >
+        Next
+      </button>
+    </div>
+  );
+}
